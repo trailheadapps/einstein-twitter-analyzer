787b77f70aac5d78d40850a73d790bd1f94ec556
Christophe Coenraets <ccoenraets@gmail.com>
2018-07-23 12:56:14 -0400

    changinh local port number

--- server.js
+++ server.js
@@ -25,4 +25,4 @@ stream.on('tweet', function (tweet) {
 http.createServer(function(request, response) {
     response.writeHead(200, {'Content-Type': 'text/plain'});
     response.end('Ok, dyno is awake.');
-}).listen(process.env.PORT || 5000);
+}).listen(process.env.PORT || 5050);
